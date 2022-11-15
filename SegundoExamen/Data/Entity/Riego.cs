260f5bdb6e4bfb9d58d3f0dea99482c74d62329d
KEVINANTHONYBARRIOS <kevin133557@gmail.com>
2022-11-15 10:04:56 -0400

    avanse 2

--- SegundoExamen/Data/Entity/Riego.cs
+++ SegundoExamen/Data/Entity/Riego.cs
@@ -1,6 +1,25 @@
-﻿namespace SegundoExamen.Data.Entity
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace SegundoExamen.Data.Entity
 {
     public class Riego
     {
+
+        public int Id { get; set; }
+
+        [Display(Name = "Costo de la Regadera")]
+        public decimal cost { get; set; }
+
+        [Display(Name = "Fecha de Riego")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "La Planta se esta secando ")]
+        public string ps { get; set; } = null!;
+
+
+        [Display(Name = "Se riega con poca agua o mucha")]
+
+        public string irrigation { get; set; }
     }
 }
